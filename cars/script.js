61e14f2c103c39917c497a4694d9ac6d27542f3c
Altair "juninh0bw" Godoi <100811728+juninh0-dev@users.noreply.github.com>
2024-01-31 14:55:21 -0300

    Update - 1.1

---
+++ cars/script.js
@@ -0,0 +1,23 @@
+//let count = 1;
+//document.getElementById("radio1").checked = true;
+
+//setInterval( function(){
+//    nextImage()
+//}, 3000)
+
+//function nextImage(){
+//    count++;
+//    if(count>2){
+//        count = 1;
+//    }
+ 
+//    document.getElementById("radio"+count).checked = true;
+//}
+
+function login(){
+    location.href="/login.html"
+}
+
+function reservas(){
+    location.href="/MinhasReservas.html"
+}
